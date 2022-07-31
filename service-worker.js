fcbb5f32d99c3650f89d88c251efa4e006e6e3b4
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-31 20:06:15 +0800

    Sun Jul 31 08:06:14 PM CST 2022

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const RUNTIME = 'runtime';
-const PRECACHE = 'precache-v2021-0615-181415'
+const PRECACHE = 'precache-v2021-0615-181416'
       
 /**
  * How to build cache list?
